817659cd56398e421e9c485832b014b9d3b6e459
Brendan T White <brendan@thespia.com>
2023-05-14 19:30:27 +0930

    Updated UI

--- src/koshin/Koshin.java
+++ src/koshin/Koshin.java
@@ -37,11 +37,19 @@ public class Koshin extends javax.swing.JFrame {
         customDirectoryPathLabel = new javax.swing.JLabel();
         customDirectoryPathTextField = new javax.swing.JTextField();
         customDirectoryPathSelectButton = new javax.swing.JButton();
-        jSeparator1 = new javax.swing.JSeparator();
-        updateCustomFilesProgressBarLabel = new javax.swing.JLabel();
-        updateCustomFilesProgressBar = new javax.swing.JProgressBar();
-        updateManifestFileProgressBarLabel = new javax.swing.JLabel();
-        updateManifestFileProgressBar = new javax.swing.JProgressBar();
+        customDirectorySeparator = new javax.swing.JSeparator();
+        custCheckBox = new javax.swing.JCheckBox();
+        custProgressBar = new javax.swing.JProgressBar();
+        custSeparator = new javax.swing.JSeparator();
+        defCheckBox = new javax.swing.JCheckBox();
+        defProgressBar = new javax.swing.JProgressBar();
+        defSeparator = new javax.swing.JSeparator();
+        distCheckBox = new javax.swing.JCheckBox();
+        distProgressBar = new javax.swing.JProgressBar();
+        distSeparator = new javax.swing.JSeparator();
+        manifestCheckBox = new javax.swing.JCheckBox();
+        manifestProgressBar = new javax.swing.JProgressBar();
+        manifestSeparator = new javax.swing.JSeparator();
         startButton = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -62,11 +70,32 @@ public class Koshin extends javax.swing.JFrame {
             }
         });
 
-        updateCustomFilesProgressBarLabel.setLabelFor(updateCustomFilesProgressBar);
-        updateCustomFilesProgressBarLabel.setText("Update Custom Files");
+        custCheckBox.setSelected(true);
+        custCheckBox.setText("Refresh Custom Files");
+        custCheckBox.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                custCheckBoxActionPerformed(evt);
+            }
+        });
 
-        updateManifestFileProgressBarLabel.setLabelFor(updateManifestFileProgressBar);
-        updateManifestFileProgressBarLabel.setText("Update Manifest File");
+        custProgressBar.setMixingCutoutShape(null);
+
+        defCheckBox.setText("Copy Default Files");
+        defCheckBox.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                defCheckBoxActionPerformed(evt);
+            }
+        });
+
+        distCheckBox.setText("Trim Distribution Files");
+
+        manifestCheckBox.setSelected(true);
+        manifestCheckBox.setText("Refresh Manifest File");
+        manifestCheckBox.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                manifestCheckBoxActionPerformed(evt);
+            }
+        });
 
         startButton.setText("Start");
         startButton.addActionListener(new java.awt.event.ActionListener() {
@@ -79,24 +108,52 @@ public class Koshin extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addGroup(layout.createSequentialGroup()
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                         .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(startButton, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(15, 15, 15)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(updateManifestFileProgressBar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(jSeparator1)
                             .addComponent(customDirectoryPathLabel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                             .addGroup(layout.createSequentialGroup()
                                 .addComponent(customDirectoryPathTextField, javax.swing.GroupLayout.DEFAULT_SIZE, 548, Short.MAX_VALUE)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(customDirectoryPathSelectButton, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addComponent(updateCustomFilesProgressBarLabel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(updateCustomFilesProgressBar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(updateManifestFileProgressBarLabel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+                                .addComponent(customDirectoryPathSelectButton, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                    .addComponent(customDirectorySeparator)
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(custSeparator))
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(defSeparator))
+                    .addComponent(distCheckBox, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(distSeparator)
+                    .addComponent(manifestCheckBox, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(manifestSeparator))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(23, 23, 23)
+                        .addComponent(manifestProgressBar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(23, 23, 23)
+                        .addComponent(distProgressBar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(23, 23, 23)
+                                .addComponent(defProgressBar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                            .addComponent(defCheckBox, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(23, 23, 23)
+                                .addComponent(custProgressBar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                            .addComponent(custCheckBox, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
@@ -108,19 +165,35 @@ public class Koshin extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(customDirectoryPathTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(customDirectoryPathSelectButton))
-                .addGap(27, 27, 27)
-                .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addComponent(updateCustomFilesProgressBarLabel)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(updateCustomFilesProgressBar, javax.swing.GroupLayout.PREFERRED_SIZE, 22, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addComponent(updateManifestFileProgressBarLabel)
+                .addComponent(customDirectorySeparator, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(custCheckBox)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(updateManifestFileProgressBar, javax.swing.GroupLayout.PREFERRED_SIZE, 22, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 19, Short.MAX_VALUE)
+                .addComponent(custProgressBar, javax.swing.GroupLayout.PREFERRED_SIZE, 22, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(custSeparator, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(defCheckBox)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(defProgressBar, javax.swing.GroupLayout.PREFERRED_SIZE, 22, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(defSeparator, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(distCheckBox)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(distProgressBar, javax.swing.GroupLayout.PREFERRED_SIZE, 22, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(distSeparator, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(manifestCheckBox)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(manifestProgressBar, javax.swing.GroupLayout.PREFERRED_SIZE, 22, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(manifestSeparator, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(startButton)
-                .addContainerGap())
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
@@ -164,6 +237,18 @@ public class Koshin extends javax.swing.JFrame {
 
     }//GEN-LAST:event_startButtonActionPerformed
 
+    private void custCheckBoxActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_custCheckBoxActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_custCheckBoxActionPerformed
+
+    private void manifestCheckBoxActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_manifestCheckBoxActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_manifestCheckBoxActionPerformed
+
+    private void defCheckBoxActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_defCheckBoxActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_defCheckBoxActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -186,15 +271,23 @@ public class Koshin extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JCheckBox custCheckBox;
+    private javax.swing.JProgressBar custProgressBar;
+    private javax.swing.JSeparator custSeparator;
     private javax.swing.JLabel customDirectoryPathLabel;
     private javax.swing.JButton customDirectoryPathSelectButton;
     private javax.swing.JTextField customDirectoryPathTextField;
-    private javax.swing.JSeparator jSeparator1;
+    private javax.swing.JSeparator customDirectorySeparator;
+    private javax.swing.JCheckBox defCheckBox;
+    private javax.swing.JProgressBar defProgressBar;
+    private javax.swing.JSeparator defSeparator;
+    private javax.swing.JCheckBox distCheckBox;
+    private javax.swing.JProgressBar distProgressBar;
+    private javax.swing.JSeparator distSeparator;
+    private javax.swing.JCheckBox manifestCheckBox;
+    private javax.swing.JProgressBar manifestProgressBar;
+    private javax.swing.JSeparator manifestSeparator;
     private javax.swing.JButton startButton;
-    private javax.swing.JProgressBar updateCustomFilesProgressBar;
-    private javax.swing.JLabel updateCustomFilesProgressBarLabel;
-    private javax.swing.JProgressBar updateManifestFileProgressBar;
-    private javax.swing.JLabel updateManifestFileProgressBarLabel;
     // End of variables declaration//GEN-END:variables
 
     public JButton getStartButton() {
@@ -206,11 +299,11 @@ public class Koshin extends javax.swing.JFrame {
     }
 
     public JProgressBar getUpdateCustomFilesProgressBar() {
-        return updateCustomFilesProgressBar;
+        return custProgressBar;
     }
 
     public JProgressBar getUpdateManifestFileProgressBar() {
-        return updateManifestFileProgressBar;
+        return manifestProgressBar;
     }
     
     public JTextField getCustomDirectoryPathTextField() {
